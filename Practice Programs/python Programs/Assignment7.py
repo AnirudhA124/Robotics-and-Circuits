68afab02163dc2f12978089a8a327a7c3c3733e5
AnirudhA1244 <agrawalanirudh18@gmail.com>
2022-11-27 00:38:14 +0530

    Adding Files

--- Practice Programs/python Programs/Assignment7.py
+++ Practice Programs/python Programs/Assignment7.py
@@ -44,8 +44,5 @@ while True:
 
     print(json.dumps(js, indent=4))
 
-    lat = js['results'][0]['geometry']['location']['lat']
-    lng = js['results'][0]['geometry']['location']['lng']
-    print('lat', lat, 'lng', lng)
-    location = js['results'][0]['formatted_address']
-    print(location)
+    pid = js['results'][0]['place_id']
+    print('Place id ',pid)
